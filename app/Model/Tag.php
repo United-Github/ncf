4bd91219110c209b54a089096f01ac07f65e7399
ragrog <ragrog216@outlook.jp>
2016-10-22 16:03:48 +0900

    タグModelの作成とタグを使用頻度順で取得するメソッドを実装

---
+++ app/Model/Tag.php
@@ -0,0 +1,32 @@
+<?php
+
+App::uses('AppModel', 'Model');
+class Tag extends AppModel {
+
+	public $useTable = 'tag';
+
+	public function getAllTag() {
+		$this->virtualFields = array(
+			'count' => 'count(LargeHeaderTag.tag_id)'
+		);
+		return $this->find('all', array(
+			'joins' => array(
+				array(
+					'type' => 'LEFT',
+					'table' => 'large_header_tag',
+					'alias' => 'LargeHeaderTag',
+					'conditions' => 'LargeHeaderTag.tag_id = Tag.id' 
+				)
+			),
+			'fields' => array(
+				'Tag.id',
+				'Tag.name',
+				'count'
+			),
+			'group' => 'Tag.id',
+			'order' => 'count DESC'
+		));
+	}
+		
+		
+}
