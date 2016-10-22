91bf9a451383c1da6b4ea8f9a70a7b200449d183
ragrog <ragrog216@outlook.jp>
2016-10-22 16:13:58 +0900

    カード一覧を取得するメソッドを実装

--- app/Model/Card.php
+++ app/Model/Card.php
@@ -11,6 +11,14 @@ class Card extends AppModel {
 		)
 	);
 
+	public function getBySHId($id) {
+		return $this->find('all', array(
+			'conditions' => array(
+				'Card.small_header_id' => $id
+			)
+		));
+	}
+
 	// // 大見出しタイトルと小見出しタイトルの取得
 	// public function getLSheader() {
 		
